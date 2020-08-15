6704a7e8686f1919eba1390a8489bcf28593a296
Young Mary <myoung@byteworks.com.ng>
2020-08-15 21:47:26 +0100

    initial commit: project set up

---
+++ src/main/java/com/fintran/demo/FinTranApplication.java
@@ -0,0 +1,13 @@
+package com.fintran.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintran/demo/FintranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintran.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
