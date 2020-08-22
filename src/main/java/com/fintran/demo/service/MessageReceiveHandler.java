2ea0409dd499aa962e73ce5cc89273925b4818be
Young Mary <myoung@byteworks.com.ng>
2020-08-22 12:31:20 +0100

    comment rabbit listener annotation

--- src/main/java/com/fintran/demo/service/MessageReceiveHandler.java
+++ src/main/java/com/fintran/demo/service/MessageReceiveHandler.java
@@ -1,7 +1,9 @@
 package com.fintran.demo.service;
 
+import com.fintran.demo.config.Constants;
 import com.fintran.demo.domain.TransactionDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +12,7 @@ public class MessageReceiveHandler {
 
     private final TransactionService transactionService;
 
+//    @RabbitListener(queues = Constants.QUEUE_NAME)
     public void handleMessage(TransactionDTO transactionDTO) {
         transactionService.createTransaction(transactionDTO);
     }
